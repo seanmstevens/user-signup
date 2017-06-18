3acdf60ea19921129d6d7cd300828bd50e351809
Sean Stevens <sstevens88@gmail.com>
2017-06-18 15:16:26 -0700

    Completed core functionality and templating

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
 import os
 import jinja2
 
@@ -8,8 +8,44 @@ app.debug = True;
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
-@app.route('/')
+@app.route('/signup', methods = ['POST', 'GET'])
 def index():
-    return "<h1>Hello World!</h1>"
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        email = request.form['email']
+        emailerror = ''
+        usererror = ''
+        passerror = ''
+        verifyerror = ''
+
+        if email:
+            if '@' not in email or '.' not in email or len(email) < 3 or len(email) > 20 or ' ' in email:
+                emailerror = "That is an invalid email"
+
+        if not username or len(username) < 3 or len(username) > 20 or ' ' in username:
+            usererror = "That's not a valid username"
+
+        if not password or len(password) < 3 or len(password) > 20 or ' ' in password:
+            passerror = "That's not a valid password"
+
+        if not verify or verify != password:
+            verifyerror = "Passwords do not match"
+
+        if not emailerror and not usererror and not passerror and not verifyerror:
+            return redirect('/welcome?username={0}'.format(username))
+        else:
+            return render_template('signup.html', username = username,
+            email = email, emailerror = emailerror, usererror = usererror,
+            passerror = passerror, verifyerror = verifyerror)
+
+    return render_template('signup.html')
+
+@app.route('/welcome')
+def welcome():
+    username = request.args.get('username')
+    return render_template('welcome.html', username = username)
 
 app.run()
